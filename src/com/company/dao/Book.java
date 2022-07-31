00aaa81e32cd497fd663820afd9b51c2fe446fd0
nikitaandreenko <n.l.andreenko@gmail.com>
2022-08-01 01:48:19 +0300

    Rename class Books for Book

---
+++ src/com/company/dao/Book.java
@@ -0,0 +1,107 @@
+package com.company.dao;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Books {
+    private Long id;
+    private String book_name;
+    private String author;
+    private String isbn;
+
+    private BigDecimal price;
+    private Integer pages;
+    private String binding;
+    private Integer year_publising;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBook_name() {
+        return book_name;
+    }
+
+    public void setBook_name(String book_name) {
+        this.book_name = book_name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Integer getPages() {
+        return pages;
+    }
+
+    public void setPages(Integer pages) {
+        this.pages = pages;
+    }
+
+    public String getBinding() {
+        return binding;
+    }
+
+    public void setBinding(String binding) {
+        this.binding = binding;
+    }
+
+    public Integer getYear_publising() {
+        return year_publising;
+    }
+
+    public void setYear_publising(Integer year_publising) {
+        this.year_publising = year_publising;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Books)) return false;
+        Books books = (Books) o;
+        return Objects.equals(id, books.id) && Objects.equals(book_name, books.book_name) && Objects.equals(author, books.author) && Objects.equals(isbn, books.isbn) && Objects.equals(price, books.price) && Objects.equals(pages, books.pages) && Objects.equals(binding, books.binding) && Objects.equals(year_publising, books.year_publising);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, book_name, author, isbn, price, pages, binding, year_publising);
+    }
+
+    @Override
+    public String toString() {
+        return "Books{" +
+                "id=" + id +
+                ", book_name='" + book_name + '\'' +
+                ", author='" + author + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", price=" + price +
+                ", pages=" + pages +
+                ", binding='" + binding + '\'' +
+                ", year_publising=" + year_publising +
+                '}';
+    }
+}
--- src/com/company/dao/Books.java
+++
@@ -1,2 +0,0 @@
-package com.company.dao;public class Books {
-}
