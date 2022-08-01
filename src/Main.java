e3e38ea03bb31c253e78bf5c4c4a8f62f330b912
nikitaandreenko <n.l.andreenko@gmail.com>
2022-08-01 15:01:15 +0300

    Create class UserService with metods for working from command line and tried this in main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,34 @@
+import com.company.dao.BookDao;
+import com.company.dao.BookDaoImpl;
+import com.company.dao.DateSourse;
+import com.company.model.Book;
+import com.company.service.UserService;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        UserService userService = new UserService();
+        BookDao bookDao = new BookDaoImpl(new DateSourse());
+        Book book = new Book();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter command");
+        userService.info(in.nextLine());
+        System.out.println("Enter name book");
+        book.setBook_name(in.nextLine());
+        System.out.println("Enter author book");
+        book.setAuthor(in.nextLine());
+        System.out.println("Enter isbn book");
+        book.setIsbn(in.nextLine());
+        System.out.println("Enter price book");
+        book.setPrice(in.nextBigDecimal());
+        System.out.println("Enter count of pages");
+        book.setBinding(in.nextLine());
+        System.out.println("Enter year_bublising");
+        book.setYear_publising(in.nextInt());
+        bookDao.create(book);
+        bookDao.update(book);
     }
 }
---
+++ src/com/company/service/UserService.java
@@ -0,0 +1,41 @@
+package com.company.service;
+
+import com.company.dao.BookDao;
+import com.company.dao.BookDaoImpl;
+import com.company.dao.DateSourse;
+import com.company.model.Book;
+
+import java.util.List;
+
+public class UserService {
+    private BookDao bookDao = new BookDaoImpl(new DateSourse());;
+
+    public void info(String command) {
+        boolean commandBol = false;
+        while (!commandBol) {
+            if (command.equalsIgnoreCase("all")) {
+                List<Book> books = bookDao.getAll();
+                books.forEach(book ->
+                        System.out.println(book.getId() + " " + book.getBook_name() + " " + book.getAuthor() + " " + book.getYear_publising()));
+                commandBol = true;
+            } else if (command.contains("get")) {
+                String[] words = command.split(" ");
+                String firstCommandUser = words[0];
+                Long secondCommandUser = Long.parseLong(words[1]);
+                Book book = bookDao.getById(secondCommandUser);
+                System.out.println(book);
+                commandBol = true;
+            } else if (command.contains("delete")) {
+                String[] words = command.split(" ");
+                String firstCommandUser = words[0];
+                Long secondCommandUser = Long.parseLong(words[1]);
+                System.out.println(bookDao.delete(secondCommandUser));
+                commandBol = true;
+            } else if (command.equalsIgnoreCase("exit")) {
+                System.out.println("End app");
+                commandBol = true;
+            }
+        }
+    }
+
+}
