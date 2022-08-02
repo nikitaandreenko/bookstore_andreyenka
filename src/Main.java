c5d9b8ed8bcb92cf94ba2bcb7ac52b19c2b3dcf8
nikitaandreenko <n.l.andreenko@gmail.com>
2022-08-02 23:56:31 +0300

    Rename class UserService for ControllerBook

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import com.company.dao.BookDao;
 import com.company.dao.BookDaoImpl;
 import com.company.dao.DateSourсe;
 import com.company.model.Book;
-import com.company.service.UserService;
+import com.company.controller.BookController;
 
 import java.util.Scanner;
 
@@ -10,24 +10,24 @@ public class Main {
     public static void main(String[] args) {
         DateSourсe dateSourсe = new DateSourсe();
         BookDao bookDao = new BookDaoImpl(dateSourсe);
-        UserService userService = new UserService(bookDao);
+        BookController bookController = new BookController(bookDao);
         Book book = new Book();
         Scanner in = new Scanner(System.in);
         System.out.println("Enter command");
-        userService.info(in.nextLine());
-//        System.out.println("Enter name book");
-//        book.setBook_name(in.nextLine());
-//        System.out.println("Enter author book");
-//        book.setAuthor(in.nextLine());
-//        System.out.println("Enter isbn book");
-//        book.setIsbn(in.nextLine());
-//        System.out.println("Enter price book");
-//        book.setPrice(in.nextBigDecimal());
-//        System.out.println("Enter count of pages");
-//        book.setBinding(in.nextLine());
-//        System.out.println("Enter year_bublising");
-//        book.setYear_publising(in.nextInt());
-//        bookDao.create(book);
-//        bookDao.update(book);
+        bookController.info(in.nextLine());
+        System.out.println("Enter name book");
+        book.setBook_name(in.nextLine());
+        System.out.println("Enter author book");
+        book.setAuthor(in.nextLine());
+        System.out.println("Enter isbn book");
+        book.setIsbn(in.nextLine());
+        System.out.println("Enter price book");
+        book.setPrice(in.nextBigDecimal());
+        System.out.println("Enter count of pages");
+        book.setBinding(in.nextLine());
+        System.out.println("Enter year_bublising");
+        book.setYear_publising(in.nextInt());
+        bookDao.create(book);
+        bookDao.update(book);
     }
 }
---
+++ src/com/company/controller/BookController.java
@@ -0,0 +1,60 @@
+package com.company.controller;
+
+import com.company.dao.BookDao;
+import com.company.model.Book;
+
+import java.util.List;
+
+public class BookController {
+    private BookDao bookDao;
+
+    public BookController(BookDao bookDao) {
+        this.bookDao = bookDao;
+    }
+    public void info(String command) {
+        boolean commandBol = false;
+        while (!commandBol) {
+            if (command.contains("all")) {
+                commandBol = all();
+            } else if (command.contains("get")) {
+                commandBol = get(command);
+            } else if (command.contains("delete")) {
+                commandBol = delete(command);
+            } else if (command.equalsIgnoreCase("exit")) {
+                System.out.println("End app");
+                commandBol = true;
+            }
+        }
+    }
+
+    private boolean delete(String command) {
+        boolean commandBol;
+        String[] words = command.split(" ");
+        String firstCommandUser = words[0];
+        Long secondCommandUser = Long.parseLong(words[1]);
+        System.out.println(bookDao.delete(secondCommandUser));
+        commandBol = true;
+        return commandBol;
+    }
+
+    private boolean get(String command) {
+        boolean commandBol;
+        String[] words = command.split(" ");
+        String firstCommandUser = words[0];
+        Long secondCommandUser = Long.parseLong(words[1]);
+        Book book = bookDao.getById(secondCommandUser);
+        System.out.println(book);
+        commandBol = true;
+        return commandBol;
+    }
+
+    private boolean all() {
+        boolean commandBol;
+        List<Book> books = bookDao.getAll();
+        books.forEach(book ->
+                System.out.println(book.getId() + " " + book.getBook_name() + " " + book.getAuthor() + " "
+                        + book.getYear_publising()));
+        commandBol = true;
+        return commandBol;
+    }
+}
