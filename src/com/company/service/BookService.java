512715d83d407ed66f665c5a9bf1af8337e6ec8d
nikitaandreenko <n.l.andreenko@gmail.com>
2022-08-03 20:19:30 +0300

    Realize base CRUD metods in BookService

--- src/com/company/service/BookService.java
+++ src/com/company/service/BookService.java
@@ -1,6 +1,9 @@
 package com.company.service;
 
 import com.company.dao.BookDaoImpl;
+import com.company.entity.Book;
+
+import java.util.List;
 
 public class BookService {
 
@@ -9,6 +12,32 @@ public class BookService {
     public BookService (){
         bookDao = new BookDaoImpl();
     }
+    public Book create(Book book) {
+        return bookDao.create(book);
+    }
+    public Book getById(Long id){
+        return bookDao.getById(id);
+    }
+    public Book getByIsbn(String isbn){
+        return bookDao.getByIsbn(isbn);
+    }
+
+    public List<Book> getAll(){
+        return bookDao.getAll();
+    }
+    public List<Book> getByAuthor(String author){
+        return bookDao.getByAuthor(author);
+    }
+    public int countAllBooks(){
+        return bookDao.countAllBooks();
+    }
+    public void delete(Long id){
+        bookDao.delete(id);
+    }
+
+
+
+
 
 
 }
